37c8b6d3397f13f62dc6af20daef0c767996efc4
Darrel Jiang <darrel.jiang.dev@gmail.com>
2019-06-02 10:05:11 -0500

    Added express to the app, organized directory

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Hello, this is Darrel Jiang's professional site
+      Hello, this is Darrel Jiang's professional site. Welcome.
     </div>
   )
 }

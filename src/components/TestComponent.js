043564f730e5b330acbeebddc59a8c073eed3e9f
Darrel Jiang <darrel.jiang.dev@gmail.com>
2019-12-16 23:18:59 -0600

    Added a test route and component

---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestComponent = () => {
+  return (
+    <React.Fragment>
+      Hello World
+    </React.Fragment>
+  )
+}
+
+export default TestComponent
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home'
 import Navbar from '../components/Navbar.js'
 import React from 'react'
 import SpaceX from '../components/SpaceX'
+import Test from '../components/TestComponent'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route component={Home} exact path='/' />
         <Route component={SpaceX} path='/spacex' />
+        <Route component={Test} path='/test' />
       </Switch>
     </Router>
   )

dcbd2cfd9e2c1fbe9241e19b40481da8b24c9a84
Darrel Jiang <darrel.jiang.dev@gmail.com>
2020-03-01 20:07:52 -0600

    updated responsiveness for smaller screens and also updated font size on the exp component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ const Header = () => (
   <div className={styles['header']}>
     <div className={styles['header__text-box']}>
       <div className={styles['header__title']}>
-        <div className={styles['header__title--left']}>Hi,</div>
+        <div className={styles['header__title--left']}>Hi!</div>
         <div className={styles['header__title--right']}>I&apos;m Darrel</div>
       </div>
       <div className={styles['header__subtitle']}>

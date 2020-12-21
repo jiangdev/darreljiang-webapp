1a21be73480d27e38d89a14d2f5944ed324c3b5c
Darrel Jiang <darrel.jiang.dev@gmail.com>
2020-12-21 11:25:31 -0600

    new build using parcel bundler

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router, Switch, Route,
-} from 'react-router-dom';
+import React from "react"
+import { render } from "react-dom"
 
-import App from './components/App';
-import Header from './components/Header';
-
-import './theme/main.scss';
-
-ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/header" component={Header} />
-    </Switch>
-  </Router>,
-  document.getElementById('root'),
-);
+render(<h1>Hello World</h1>, document.getElementById("root"))
